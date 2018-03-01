f77e6e73e15749f4aa40b72f160759685b4d8402
Yang Jingyu <30931540+shendeguize@users.noreply.github.com>
2018-03-01 21:48:30 +0800

    Sol,Dom

--- main/EtymoList.py
+++ main/EtymoList.py
@@ -1,5 +1,12 @@
 EtymoList = {
-  'sol':['Sun','solar','solstice','parasol','insolate','solarium'],
+  'di':['day','diary','meridian','dismal'],
   
+  'sol':['sun','solar','solstice','parasol','insolate','solarium'],
+  'domina':['lady','donna','belladonna','dame','damsel','demoiselle','madenoiselle'],
+  'dominus':['master','dominate','predominate','domination','dominator','dominatrix','domineer','domain'],
+  'dome':['house','domestic','domesticate','domicile','domatium'],
+  
+  'luna':['moon','lunar'],
+
  
 }

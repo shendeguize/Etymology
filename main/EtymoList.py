052f7991b0cbc1740b930419a87cb3bbe0f371ff
Yang Jingyu <30931540+shendeguize@users.noreply.github.com>
2018-03-06 18:13:07 +0800

    2018.03.06

--- main/EtymoList.py
+++ main/EtymoList.py
@@ -1,7 +1,8 @@
-EtymoList = {
+EtymoList�= {
   'di':['day','diary','meridian','dismal'],
   
   'sol':['sun','solar','solstice','parasol','insolate','solarium'],
+
   'domina':['lady','donna','belladonna','dame','damsel','demoiselle','madenoiselle'],
   'dominus':['master','dominate','predominate','domination','dominator','dominatrix','domineer','domain'],
   'dome':['house','domestic','domesticate','domicile','domatium'],
@@ -9,7 +10,23 @@ EtymoList = {
   'luna':['moon','lunar','perilune','apolune','lunatic'],
   'luc':['light','luciferous','lucid','lucigen','lucent','noctilucent','translucent','lumen','luminary','luminous','luminance','luminferous','transilluminae','illuminate','illuminant','illumine','luminescence','autoluminescence','bioluminescence'],
 
+  'mas':['male','masculine','male','macho','emasculate'],
+
+  'merc':['trade','commerce','commercial','merchant','merchandise','mercenary','mercy','market'],
+
+  'di':['god','deity','deify','deist','divine'],
+  'pater':['father','compatriot','patriarch','patricide','paternoster'],
+
+  'chronos':['time','chronology','anachronism','diachronic','synchronous','asynchronous'],
   
+
+
+
+
+
+
+
+
 
  
 }

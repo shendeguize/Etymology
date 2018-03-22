f83912894b22ae3f17909868c0d06732a4f8c0e2
Yang Jingyu <30931540+shendeguize@users.noreply.github.com>
2018-03-22 15:33:05 +0800

    2018.03.22

--- main/EtymoList.py
+++ main/EtymoList.py
@@ -1,40 +1,27 @@
 EtymoList = {
-  'di':['day','diary','meridian','dismal'],
-  
+  'di':['day','diary','meridian','dismal'],  
   'sol':['sun','solar','solstice','parasol','insolate','solarium'],
-
   'domina':['lady','donna','belladonna','dame','damsel','demoiselle','madenoiselle'],
   'dominus':['master','dominate','predominate','domination','dominator','dominatrix','domineer','domain'],
-  'dome':['house','domestic','domesticate','domicile','domatium'],
-  
+  'dome':['house','domestic','domesticate','domicile','domatium'],  
   'luna':['moon','lunar','perilune','apolune','lunatic'],
   'luc':['light','luciferous','lucid','lucigen','lucent','noctilucent','translucent','lumen','luminary','luminous','luminance','luminferous','transilluminae','illuminate','illuminant','illumine','luminescence','autoluminescence','bioluminescence'],
-
   'mas':['male','masculine','male','macho','emasculate'],
-
   'merc':['trade','commerce','commercial','merchant','merchandise','mercenary','mercy','market'],
-
   'di':['god','deity','deify','deist','divine'],
   'pater':['father','compatriot','patriarch','patricide','paternoster'],
-
-  'chronos':['time','chronology','anachronism','diachronic','synchronous','asynchronous'],
-   
+  'chronos':['time','chronology','anachronism','diachronic','synchronous','asynchronous'],   
   'arctos':['north','Arctic','Antarctic'],
   'aster':['star','astronomy','astrology','astromancy','astronaut','asterisk','asteroid','disaster'],
   'stella':['star','constellation','stellar'],
   'nomos':['law','economy','autonomy','gastronomy'],
   'zoe':['zodiac','zoology','zoo','phanerozoic','paleozoic','mesozoic','cenozoic'],
-
   'galact':['milk','galaxy'],
   'lact':['milk','lactose'],
-
   'terra':['earth','Mediterranean','inter','disinter','subterrane','territory'],
   'ge':['earth','geometry','geography','geology'],
-
   'phot':['light','photograph','photon','photosphere','photogen','photosynthesis','phosphorus'],
-
   'phobos':['afraid','hydrophobia','acrophobia','panophobia'],
-
   'io':['wander','ion','ionosphere','ionogen'],
   'ops':['eye','autopsy','myopia','hyperopia','xanthopia','optics','optician'],
   'aqua':['water','aquarium','aqueduct','aquifer','aquacada','aqualung','aquanaut','aquaculture'],
